22534342749aa19395f4a30a49d2527f40e86395
NGOA CHARLES CEDRIC <120021231+CharlyOffset@users.noreply.github.com>
2025-05-08 10:48:12 +0100

    Update settings.py

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": 'elderco',
         "USER": 'root',
-        "PASSWORD": 'ngoa',
+        "PASSWORD": 'PASSWORD',
         "HOST": 'localhost',
         "PORT": '3306',
     }
